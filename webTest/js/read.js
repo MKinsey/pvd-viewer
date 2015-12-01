e4c8c52053a589c48dd690761e1a35070b3a3054
Michael Kinsey <dr.michael.kinsey@gmail.com>
2015-11-30 19:59:15 -0700

    properly parsing xml

--- webTest/js/read.js
+++ webTest/js/read.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
-  loadDoc("simple.vtu")
+  loadDoc("Bed000000.vtu")
 })
 function loadDoc(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       document.getElementById('a').innerHTML = "Loaded"
-      parse (xhttp)
+      parse (xhttp.responseText)
     }
   };
   xhttp.open("GET", url, true);
   xhttp.send();
 }
 
-function parse(xml){
-  xmlDoc = $.parseXML( xml ),
-  $xml = $( xmlDoc ),
-  $test = $xml.find("DataArray")
-  console.log($test);
+function parse(x){
+  xmlDoc = $.parseXML( x )
+  xml = $( xmlDoc )
+  points = xml.find('Points')
+  cells = xml.find('Cells')
+  connectivity = xml.find('[Name="connectivity"]')
+  offsets = xml.find('[Name="offsets"]')
+  types = xml.find('[Name="types"]')
+  pData = xml.find('PointData')
 }
