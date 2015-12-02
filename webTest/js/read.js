f7c30b583e0cd3616c692f45651b0890f9c7e644
CarlosAlbaladejo <carlos.alba.padilla@gmail.com>
2015-12-01 20:47:06 -0700

    Last changes

--- webTest/js/read.js
+++ webTest/js/read.js
@@ -1,3 +1,5 @@
+var geometry;
+
 $(document).ready(function(){
   camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 1e10 );
   camera.position.z = 5;
@@ -21,8 +23,8 @@ $(document).ready(function(){
   //load data
   loadDoc("Bed000000.vtu")
 
+  alert('Loaded')
   // light
-
   var dirLight = new THREE.DirectionalLight( 0xffffff );
   dirLight.position.set( 200, 200, 1000 ).normalize();
 
@@ -40,6 +42,12 @@ $(document).ready(function(){
   $("#container").append(container)
   container.appendChild( renderer.domElement );
   // add to scene
+  
+  
+  var material = new THREE.MeshLambertMaterial( { color:0xffffff, side: THREE.DoubleSide } );
+  var mesh = new THREE.Mesh( geometry, material );
+  mesh.position.setY( - 0.09 );
+  scene.add( mesh );
 
   animate()
 })
@@ -54,8 +62,8 @@ function loadDoc(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById('a').innerHTML = "Loaded"
       parse (xhttp.responseText)
+      document.getElementById('a').innerHTML = "Loaded"
     }
   };
   xhttp.open("GET", url, true);
@@ -69,24 +77,39 @@ function parse(x){
   points = xml.find('Points')
   numberOfComponents = points.find('DataArray').attr('NumberOfComponents')
   cells = xml.find('Cells')
+  numberOfCells = xml.find('Piece').attr('NumberOfCells')
   connectivity = xml.find('[Name="connectivity"]')
   offsets = xml.find('[Name="offsets"]')
   types = xml.find('[Name="types"]')
   pData = xml.find('PointData')
-  build(points, offsets, numberOfComponents)
+  alert('Building')
+  build(points, offsets, numberOfComponents, connectivity, numberOfCells)
+  alert('Builded')
 }
 
-function build(pointsXml, offsets, numberOfComponents){
-  // nPts = pointsXml.split(" ")
+function build(pointsXml, offsets, numberOfComponents, connect, nOfCells){
+  
+  var indices = [];
+  var positions = [];
+  
   if(numberOfComponents == 3){
-    points = pointsXml.text().split(' ')
+    points = pointsXml.text().split('( )+')
+    cellsArray = connect.text().split('( )+');
     nPoints = points.length
     for(i = 0; i < nPoints; i += Number(numberOfComponents)){
-      x = points[i]
-      y = points[i+1]
-      z = points[i+2]
-
+      positions.push( parseInt( points[i] ), parseInt( points[ i+1 ] ), parseInt( points[ i+2 ] ) );
+    }
+    for(var j = 0; j < nOfCells; j+=3){
+      indices.push( parseInt( cellsArray[i] ), parseInt( cellsArray[ i+1 ] ), parseInt( cellsArray[ i+2 ] ) );
     }
   }
-
+  
+  
+  geometry = new THREE.BufferGeometry();
+  geometry.setIndex( new THREE.BufferAttribute( new ( indices.length > 65535 ? Uint32Array : Uint16Array )( indices ), 1 ) );
+  geometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
+  geometry.computeVertexNormals();
+  
+  return
+  
 }
