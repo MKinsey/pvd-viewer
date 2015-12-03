8ac8dd8405833176acbf186c2d3b62ae8992e20b
CarlosAlbaladejo <carlos.alba.padilla@gmail.com>
2015-12-02 18:54:03 -0700

    plotting sth

--- BunnyExample/js/VTKNewLoader.js
+++ BunnyExample/js/VTKNewLoader.js
@@ -36,87 +36,42 @@ THREE.VTKNewLoader.prototype = {
 
 	parse: function ( data ) {
 
+		var xmlDoc = $.parseXML( data )
+        var xml = $( xmlDoc )
+        // get specific data
+        var pts = xml.find('Points')
+        var numberOfComponents = pts.find('DataArray').attr('NumberOfComponents')
+        var cells = xml.find('Cells')
+        var numberOfCells = xml.find('Piece').attr('NumberOfCells')
+        var connectivity = xml.find('[Name="connectivity"]')
+        var offsets = xml.find('[Name="offsets"]')
+        var types = xml.find('[Name="types"]')
+        var pData = xml.find('PointData')
+		
+		
 		var indices = [];
-		var positions = [];
-
-		var result;
-
-		// float float float
-
-		var pat3Floats = /([\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\-]?[\d]+[\.]?[\d|\-|e]*)[ ]+([\-]?[\d]+[\.]?[\d|\-|e]*)/g;
-		var patTriangle = /^3[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)/;
-		var patQuad = /^4[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)[ ]+([\d]+)/;
-		var patPOINTS = /^POINTS /;
-		var patPOLYGONS = /^POLYGONS /;
-		var inPointsSection = false;
-		var inPolygonsSection = false;
-
-		xmlDoc = $.parseXML( data )
-		xml = $( xmlDoc )
-		points = xml.find('Points')
-		cells = xml.find('Cells')
-		connectivity = xml.find('[Name="connectivity"]')
-		offsets = xml.find('[Name="offsets"]')
-		types = xml.find('[Name="types"]')
-		pData = xml.find('PointData')
-		
-		
-		//var lines = data.split('\n');
-		/*for ( var i = 0; i < lines.length; ++i ) {
-
-			line = lines[i];
-
-			if ( inPointsSection ) {
-
-				// get the vertices
-
-				while ( ( result = pat3Floats.exec( line ) ) !== null ) {
-					positions.push( parseFloat( result[ 1 ] ), parseFloat( result[ 2 ] ), parseFloat( result[ 3 ] ) );
-				}
-			}
-			else if ( inPolygonsSection ) {
-
-				result = patTriangle.exec(line);
-
-				if ( result !== null ) {
-
-					// 3 int int int
-					// triangle
-
-					indices.push( parseInt( result[ 1 ] ), parseInt( result[ 2 ] ), parseInt( result[ 3 ] ) );
-				}
-				else {
-
-					result = patQuad.exec(line);
-
-					if ( result !== null ) {
-
-						// 4 int int int int
-						// break quad into two triangles
-
-						indices.push( parseInt( result[ 1 ] ), parseInt( result[ 2 ] ), parseInt( result[ 4 ] ) );
-						indices.push( parseInt( result[ 2 ] ), parseInt( result[ 3 ] ), parseInt( result[ 4 ] ) );
-					}
-
-				}
-
-			}
-
-			if ( patPOLYGONS.exec(line) !== null ) {
-				inPointsSection = false;
-				inPolygonsSection = true;
-			}
-			if ( patPOINTS.exec(line) !== null ) {
-				inPolygonsSection = false;
-				inPointsSection = true;
-			}
-		}*/
-
-		var geometry = new THREE.BufferGeometry();
-//		geometry.setIndex( new THREE.BufferAttribute( new ( indices.length > 65535 ? Uint32Array : Uint16Array )( indices ), 1 ) );
-//		geometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
-
-		return geometry;
+        var positions = [];
+        var decimal = /(-?\d+\.?\d*)/g
+      
+        if(numberOfComponents == 3){
+            points = pts.text().match(decimal)
+            cellsArray = connectivity.text().match(decimal)
+            nPoints = points.length
+            for(i = 0; i < nPoints; i += 3){
+                positions.push( parseFloat( points[i] ), parseFloat( points[ i+1 ] ), parseFloat( points[ i+2 ] ) );
+            }
+            for(var j = 0; j < numberOfCells; j+=3){
+                indices.push( parseFloat( cellsArray[j] ), parseFloat( cellsArray[ j+1 ] ), parseFloat( cellsArray[ j+2 ] ) );
+            }
+        }
+      
+      
+        var geometry = new THREE.BufferGeometry();
+        geometry.setIndex( new THREE.BufferAttribute( new ( indices.length > 65535 ? Uint32Array : Uint16Array )( indices ), 1 ) );
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
+        
+      
+        return geometry
 
 	}
 
